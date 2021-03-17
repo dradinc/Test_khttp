25820db36daba0fe09b5a820b2ebe1f988e860b9
Artem <dradinc@yandex.ru>
2021-03-17 18:11:32 +0300

    ssss

--- app/src/main/java/com/example/test_khttp/MainActivity.kt
+++ app/src/main/java/com/example/test_khttp/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.annotation.RequiresApi
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import okhttp3.*
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.w3c.dom.Text
 import java.io.IOException
@@ -19,38 +19,125 @@ import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+    // Переменные для работы с HTTP запросами
+    var token : Int? = null
+    val client = OkHttpClient()
+    val gson = Gson()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+
+    // Тестирование GET запроса
+    @Override
     fun Click_SearchMovie (view: View) {
         var movieID : String = findViewById<EditText>(R.id.idMovie).text.toString()
         run(movieID)
     }
-
-    private val client = OkHttpClient()
+    @Override
     fun run(movieID : String) {
-        val request = Request.Builder()
-                .url("http://cinema.areas.su/movies/$movieID")
-                .build()
+
+        // Тест GET запроса
+        val request = Request.Builder().url("http://cinema.areas.su/movies/$movieID").build()
+
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
+                var result : String = e.toString()
+                runOnUiThread {
+                    findViewById<TextView>(R.id.Test2_textView).text = result
+                }
             }
+
             override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
+                var result : String = response.toString()
+                runOnUiThread {
+                    findViewById<TextView>(R.id.Test2_textView).text = result
+
                     val responseBody = response.body?.string()
-                    var gson = Gson()
                     val movieInfo : Movie_Info = gson.fromJson(responseBody, Movie_Info::class.java)
-                    val TestString: Movie_Info = gson.fromJson(responseBody, Movie_Info::class.java)
-                    runOnUiThread {
-                        //findViewById<TextView>(R.id.Test2_textView).text = responseBody
-                        findViewById<TextView>(R.id.Test2_textView).text = TestString.toString()
-                        findViewById<ImageView>(R.id.MoviePoster).setImageDrawable(movieInfo.getPoster())
+                    runOnUiThread{
+                        findViewById<TextView>(R.id.Test2_textView1).text = movieInfo.toString()
+                    }
+                }
+            }
+
+        })
+    }
+
+
+    // Тестирование POST запроса
+    @Override
+    fun buttonAuth_onClick(view : View) {
+        // Тест POST запроса
+        val requestBody  = RequestBody.create("application/json; charset=utf-8".toMediaTypeOrNull(), "{\"email\":\"vasya@mail.com\", \"password\":\"qwerty\"}")
+        val requestPOST = Request.Builder().url("http://cinema.areas.su/auth/login").post(requestBody).build()
+        client.newCall(requestPOST).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                var result : String = e.toString()
+                runOnUiThread {
+                    findViewById<TextView>(R.id.testPOST).text = result
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                var result : String = response.toString()
+                runOnUiThread {
+                    findViewById<TextView>(R.id.testPOST).text = result
+
+                    val responseBody = response.body?.string()
+                    val loginUser : loginUser = gson.fromJson(responseBody, loginUser::class.java)
+                    runOnUiThread{
+                        token = loginUser.token
+                        findViewById<TextView>(R.id.testPost2).text = loginUser.toString()
+                        findViewById<Button>(R.id.ButtonToken).visibility = View.VISIBLE
                     }
                 }
             }
         })
+    }
+
+
+    // Тестирование использования токена
+    @Override
+    fun buttonToken_onClick(view : View) {
+        // Тест закрытого запроса с использованием токена
+        val request_token = Request.Builder()
+            .url("http://cinema.areas.su/user")
+            .addHeader("Authorization", "Bearer " + token)
+            .build()
+        client.newCall(request_token).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                var result : String = e.toString()
+                runOnUiThread {
+                    findViewById<TextView>(R.id.testToken).text = result
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                var result : String = response.toString()
+                runOnUiThread {
+                    findViewById<TextView>(R.id.testToken).text = result
+                }
+                val turnsType = object : TypeToken<List<Users>>() {}.type
+                val responseBody = response.body?.string()
+                val dataUsers = gson.fromJson<List<Users>>(responseBody, turnsType)
+                runOnUiThread {
+                    for (i in dataUsers.indices) {
+                        findViewById<TextView>(R.id.testToken2).text = dataUsers[0].toString()
+                    }
+                }
+            }
+        })
+    }
+
+
+
+    //  Получение картинки
+    @Override
+    fun image_onClick(view : View) {
+
     }
 }
---
+++ app/src/main/java/com/example/test_khttp/dataUsers.kt
@@ -0,0 +1,18 @@
+package com.example.test_khttp
+
+class Users (val userID : String, val firstName : String, val lastName : String, val email : String, val avatar : String) {
+
+    override fun toString(): String {
+        return "userID: ${this.userID} \n" +
+                "firstName: ${this.firstName} \n" +
+                "lastName: ${this.lastName} \n" +
+                "email: ${this.email}" +
+                "avatar: ${this.avatar}"
+    }
+}
+
+class dataUsers(val Users : List<Map<String, String>>) {
+    override fun toString(): String {
+        return "User: ${this.Users} \n"
+    }
+}
